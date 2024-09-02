f8a8e09e917fee4d8681965f3e7b59bcb3ed902c
Tomer <tomer.blumental@gmail.com>
2024-09-02 13:38:06 +0300

    02.09.24

--- src/FileCabinet/SuiteScripts/SL Payment Screen.js
+++ src/FileCabinet/SuiteScripts/SL Payment Screen.js
@@ -1,9 +1,65 @@
 /**
- * @NApiVersion 2.x
+ * @NApiVersion 2.1
  * @NScriptType Suitelet
  */
-define(['N/ui/serverWidget', 'N/search', 'N/redirect'], 
-    function(serverWidget, search, redirect) {
+define(['N/ui/serverWidget', 'N/search', 'N/url', 'N/redirect'], 
+    function(serverWidget, search, url, redirect) {
+
+        function quoteAttr(s) {
+            return ("" + s).replace(/&/g, "&amp;").replace(/'/g, "&apos;").replace(/"/g, "&quot;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+        }
+
+        function toBooleanString(boolean) {
+            return boolean ? "true" : "false";
+        }
+
+        function createReactApp(form, isConnected, hasConnectionError, processIsRunning) {
+            var host = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : "";
+
+            var styleTag = '<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />';
+            var styleField = form.addField({
+                id: "custpage_appstylefield",
+                type: serverWidget.FieldType.INLINEHTML,
+                label: "App Style"
+            });
+            styleField.defaultValue = styleTag;
+
+            var appTag = "<div id=\"netsuite_ssp\" data-host=\"".concat(host, "\" data-is-connected=\"").concat(toBooleanString(isConnected), "\" data-is-connection-error=\"").concat(toBooleanString(hasConnectionError), "\" data-is-process-running=\"").concat(toBooleanString(processIsRunning), "\"></div>");
+            var appField = form.addField({
+                id: "custpage_appcontainerfield",
+                type: serverWidget.FieldType.INLINEHTML,
+                label: "App Container"
+            });
+            appField.defaultValue = appTag;
+            appField.updateLayoutType({
+                layoutType: serverWidget.FieldLayoutType.OUTSIDEBELOW
+            });
+        }
+
+        function initApp(context, form, _ref) {
+            var accessTitle = _ref.accessTitle,
+                loadAdditionalData = _ref.loadAdditionalData || function () { return true; },
+                hasAccess = _ref.hasAccess || function () { return false; },
+                isProcessRunning = _ref.isProcessRunning || function () { return false; },
+                addAdditionalFormFields = _ref.addAdditionalFormFields || function () { return false; };
+
+            createReactApp(form, true, false, isProcessRunning());
+
+            if (!loadAdditionalData(form, function (e) {
+                return showConnectionError(context, form, e);
+            })) {
+                return;
+            }
+
+            if (!hasAccess()) {
+                form = displayNotAvailableErrorScreen(form, accessTitle);
+                context.response.writePage(form);
+                return;
+            }
+
+            // Ensure "Next" button is added correctly
+            addAdditionalFormFields(form);
+        }
 
         function onRequest(context) {
             if (context.request.method === 'GET') {
@@ -14,25 +70,48 @@ define(['N/ui/serverWidget', 'N/search', 'N/redirect'],
                     title: 'Payment Screen'
                 });
 
-                // Attach Client Script using the module path
-                form.clientScriptModulePath = './cs_summary_screen.js'; 
+                form.clientScriptModulePath = './cs_summary_screen.js';
+
+                initApp(context, form, {
+                    accessTitle: 'Bill Payments',
+                    loadAdditionalData: function(form, showConnectionError) {
+                        try {
+                            return true;
+                        } catch (e) {
+                            showConnectionError(e);
+                            return false;
+                        }
+                    },
+                    hasAccess: function() {
+                        return true;
+                    },
+                    isProcessRunning: function() {
+                        return false;
+                    },
+                    addAdditionalFormFields: function(form) {
+                        // Add the "Next" button explicitly here
+                        form.addButton({
+                            id: 'custpage_next',
+                            label: 'Next',
+                            functionName: 'onNextClick'
+                        });
+                    }
+                });
 
                 var sublist = form.addSublist({
                     id: 'custpage_payment_sublist',
-                    type: serverWidget.SublistType.INLINEEDITOR, // Back to INLINEEDITOR to allow editing
+                    type: serverWidget.SublistType.EDITOR,
                     label: 'Payment Selection'
                 });
 
-                // Checkbox field
                 sublist.addField({
-                    id: 'custpage_select',
+                    id: 'custpage_selectfield',
                     type: serverWidget.FieldType.CHECKBOX,
                     label: 'Select'
                 });
 
-                // Other fields...
                 sublist.addField({
-                    id: 'custpage_date',
+                    id: 'custpage_datefield',
                     type: serverWidget.FieldType.DATE,
                     label: 'Date'
                 }).updateDisplayType({
@@ -40,7 +119,7 @@ define(['N/ui/serverWidget', 'N/search', 'N/redirect'],
                 });
 
                 sublist.addField({
-                    id: 'custpage_name',
+                    id: 'custpage_namefield',
                     type: serverWidget.FieldType.TEXT,
                     label: 'Name'
                 }).updateDisplayType({
@@ -48,7 +127,7 @@ define(['N/ui/serverWidget', 'N/search', 'N/redirect'],
                 });
 
                 sublist.addField({
-                    id: 'custpage_document_number',
+                    id: 'custpage_documentnumberfield',
                     type: serverWidget.FieldType.TEXT,
                     label: 'Document Number'
                 }).updateDisplayType({
@@ -56,7 +135,7 @@ define(['N/ui/serverWidget', 'N/search', 'N/redirect'],
                 });
 
                 sublist.addField({
-                    id: 'custpage_currency',
+                    id: 'custpage_currencyfield',
                     type: serverWidget.FieldType.TEXT,
                     label: 'Currency'
                 }).updateDisplayType({
@@ -64,7 +143,7 @@ define(['N/ui/serverWidget', 'N/search', 'N/redirect'],
                 });
 
                 sublist.addField({
-                    id: 'custpage_amount',
+                    id: 'custpage_amountfield',
                     type: serverWidget.FieldType.CURRENCY,
                     label: 'Amount (Foreign Currency)'
                 }).updateDisplayType({
@@ -72,20 +151,19 @@ define(['N/ui/serverWidget', 'N/search', 'N/redirect'],
                 });
 
                 sublist.addField({
-                    id: 'custpage_payment_amount',
+                    id: 'custpage_paymentamountfield',
                     type: serverWidget.FieldType.CURRENCY,
                     label: 'Payment Amount'
                 });
 
                 sublist.addField({
-                    id: 'custpage_amount_remaining',
+                    id: 'custpage_amountremainingfield',
                     type: serverWidget.FieldType.CURRENCY,
                     label: 'Amount Remaining (Foreign Currency)'
                 }).updateDisplayType({
                     displayType: serverWidget.FieldDisplayType.DISABLED
                 });
 
-                // Load and populate sublist with data...
                 var transactionSearchObj = search.load({
                     id: 'customsearch_payment_screen_result'
                 });
@@ -112,63 +190,45 @@ define(['N/ui/serverWidget', 'N/search', 'N/redirect'],
                     var amountRemaining = amount;
 
                     sublist.setSublistValue({
-                        id: 'custpage_date',
+                        id: 'custpage_datefield',
                         line: i,
                         value: results[i].getValue('trandate')
                     });
                     sublist.setSublistValue({
-                        id: 'custpage_name',
+                        id: 'custpage_namefield',
                         line: i,
                         value: results[i].getText('entity')
                     });
                     sublist.setSublistValue({
-                        id: 'custpage_document_number',
+                        id: 'custpage_documentnumberfield',
                         line: i,
                         value: results[i].getValue('tranid')
                     });
                     sublist.setSublistValue({
-                        id: 'custpage_currency',
+                        id: 'custpage_currencyfield',
                         line: i,
                         value: results[i].getText('currency')
                     });
                     sublist.setSublistValue({
-                        id: 'custpage_amount',
+                        id: 'custpage_amountfield',
                         line: i,
                         value: amount
                     });
                     sublist.setSublistValue({
-                        id: 'custpage_payment_amount',
+                        id: 'custpage_paymentamountfield',
                         line: i,
                         value: 0
                     });
                     sublist.setSublistValue({
-                        id: 'custpage_amount_remaining',
+                        id: 'custpage_amountremainingfield',
                         line: i,
                         value: amountRemaining
                     });
                 }
 
-                // Add a field to display the Total Amount Selected
-                var totalAmountField = form.addField({
-                    id: 'custpage_total_selected_amount',
-                    type: serverWidget.FieldType.CURRENCY,
-                    label: 'Total Amount Selected'
-                });
-
-                totalAmountField.updateDisplayType({
-                    displayType: serverWidget.FieldDisplayType.INLINE // Make it non-editable
-                });
-
-                // Add the "Next" button
-                form.addButton({
-                    id: 'custpage_next',
-                    label: 'Next',
-                    functionName: 'goToSummary'
-                });
-
                 context.response.writePage(form);
             } else {
-                // Handle the POST request if needed, but this may be handled by the summary Suitelet
+                // Handle the POST request logic if needed
             }
         }
 
--- src/FileCabinet/SuiteScripts/cs_summary_screen.js
+++ src/FileCabinet/SuiteScripts/cs_summary_screen.js
@@ -1,132 +1,125 @@
 /**
- * @NApiVersion 2.x
+ * @NApiVersion 2.1
  * @NScriptType ClientScript
  */
 define(['N/url', 'N/currentRecord'], function(url, currentRecord) {
 
-    function updateTotalSelectedAmount() {
-        var record = currentRecord.get();
-        var lineCount = record.getLineCount({ sublistId: 'custpage_payment_sublist' });
-        var totalSelectedAmount = 0;
-
-        for (var i = 0; i < lineCount; i++) {
-            var isSelected = record.getSublistValue({
-                sublistId: 'custpage_payment_sublist',
-                fieldId: 'custpage_select',
-                line: i
-            });
-
-            if (isSelected) {
-                var paymentAmount = parseFloat(record.getSublistValue({
-                    sublistId: 'custpage_payment_sublist',
-                    fieldId: 'custpage_payment_amount',
-                    line: i
-                })) || 0;
-
-                totalSelectedAmount += paymentAmount;
-            }
-        }
-
-        // Update the total selected amount field on the form
-        record.setValue({
-            fieldId: 'custpage_total_selected_amount',
-            value: totalSelectedAmount.toFixed(2)
-        });
-    }
-
     function fieldChanged(context) {
+        var record = context.currentRecord;
         var sublistId = context.sublistId;
         var fieldId = context.fieldId;
 
         if (sublistId === 'custpage_payment_sublist') {
-            var paymentAmountFieldId = 'custpage_payment_amount';
-            var amountFieldId = 'custpage_amount';
-            var remainingAmountFieldId = 'custpage_amount_remaining';
-            var selectFieldId = 'custpage_select';
+            var paymentAmountFieldId = 'custpage_paymentamountfield';
+            var amountFieldId = 'custpage_amountfield';
+            var selectFieldId = 'custpage_selectfield';
+            var amountRemainingFieldId = 'custpage_amountremainingfield';
 
-            if (fieldId === paymentAmountFieldId || fieldId === selectFieldId) {
-                var paymentAmount = parseFloat(context.currentRecord.getCurrentSublistValue({
-                    sublistId: sublistId,
-                    fieldId: paymentAmountFieldId
-                })) || 0;
+            var paymentAmount = parseFloat(record.getCurrentSublistValue({
+                sublistId: sublistId,
+                fieldId: paymentAmountFieldId
+            })) || 0;
 
-                var totalAmount = parseFloat(context.currentRecord.getCurrentSublistValue({
-                    sublistId: sublistId,
-                    fieldId: amountFieldId
-                }));
+            var totalAmount = parseFloat(record.getCurrentSublistValue({
+                sublistId: sublistId,
+                fieldId: amountFieldId
+            })) || 0;
 
-                var isSelected = context.currentRecord.getCurrentSublistValue({
-                    sublistId: sublistId,
-                    fieldId: selectFieldId
-                });
+            var isSelected = record.getCurrentSublistValue({
+                sublistId: sublistId,
+                fieldId: selectFieldId
+            });
 
-                if (fieldId === paymentAmountFieldId) {
-                    // Ensure payment amount does not exceed total amount
-                    if (paymentAmount > totalAmount) {
-                        alert('Payment amount cannot exceed the total amount.');
-                        context.currentRecord.setCurrentSublistValue({
-                            sublistId: sublistId,
-                            fieldId: paymentAmountFieldId,
-                            value: totalAmount,
-                            ignoreFieldChange: true
-                        });
-                        paymentAmount = totalAmount;
-                    }
+            // Handle changes to the checkbox
+            if (fieldId === selectFieldId) {
+                if (isSelected) {
+                    // Set Payment Amount to Total Amount when selected
+                    record.setCurrentSublistValue({
+                        sublistId: sublistId,
+                        fieldId: paymentAmountFieldId,
+                        value: totalAmount,
+                        ignoreFieldChange: true
+                    });
 
-                    // Automatically select the line if payment amount is greater than 0
-                    if (paymentAmount > 0 && !isSelected) {
-                        context.currentRecord.setCurrentSublistValue({
-                            sublistId: sublistId,
-                            fieldId: selectFieldId,
-                            value: true,
-                            ignoreFieldChange: true
-                        });
-                    }
+                    // Set Amount Remaining to 0
+                    record.setCurrentSublistValue({
+                        sublistId: sublistId,
+                        fieldId: amountRemainingFieldId,
+                        value: 0,
+                        ignoreFieldChange: true
+                    });
+                } else {
+                    // Reset Payment Amount to 0 when deselected
+                    record.setCurrentSublistValue({
+                        sublistId: sublistId,
+                        fieldId: paymentAmountFieldId,
+                        value: 0,
+                        ignoreFieldChange: true
+                    });
 
-                    // Automatically deselect the line if payment amount is set to 0
-                    if (paymentAmount === 0 && isSelected) {
-                        context.currentRecord.setCurrentSublistValue({
-                            sublistId: sublistId,
-                            fieldId: selectFieldId,
-                            value: false,
-                            ignoreFieldChange: true
-                        });
-                    }
+                    // Set Amount Remaining to the Total Amount
+                    record.setCurrentSublistValue({
+                        sublistId: sublistId,
+                        fieldId: amountRemainingFieldId,
+                        value: totalAmount,
+                        ignoreFieldChange: true
+                    });
+                }
+            }
+
+            // Handle changes to the Payment Amount
+            if (fieldId === paymentAmountFieldId) {
+                if (paymentAmount > totalAmount) {
+                    alert('Payment amount cannot exceed the total amount.');
+                    paymentAmount = totalAmount;
+                    record.setCurrentSublistValue({
+                        sublistId: sublistId,
+                        fieldId: paymentAmountFieldId,
+                        value: paymentAmount,
+                        ignoreFieldChange: true
+                    });
                 }
 
-                if (fieldId === selectFieldId) {
-                    if (isSelected === true) {
-                        // Set payment amount to total amount when selected
-                        context.currentRecord.setCurrentSublistValue({
-                            sublistId: sublistId,
-                            fieldId: paymentAmountFieldId,
-                            value: totalAmount,
-                            ignoreFieldChange: true
-                        });
-                    } else {
-                        // Reset payment amount to 0 when deselected
-                        context.currentRecord.setCurrentSublistValue({
-                            sublistId: sublistId,
-                            fieldId: paymentAmountFieldId,
-                            value: 0,
-                            ignoreFieldChange: true
-                        });
-                    }
-                }
-
-                // Update remaining amount
-                var remainingAmount = totalAmount - paymentAmount;
-
-                context.currentRecord.setCurrentSublistValue({
+                // Update the Amount Remaining based on the new Payment Amount
+                var amountRemaining = totalAmount - paymentAmount;
+                record.setCurrentSublistValue({
                     sublistId: sublistId,
-                    fieldId: remainingAmountFieldId,
-                    value: remainingAmount.toFixed(2),
+                    fieldId: amountRemainingFieldId,
+                    value: amountRemaining,
                     ignoreFieldChange: true
                 });
 
-                // Update the total selected amount
-                updateTotalSelectedAmount();
+                // Automatically select the checkbox if payment amount > 0
+                if (paymentAmount > 0 && !isSelected) {
+                    record.setCurrentSublistValue({
+                        sublistId: sublistId,
+                        fieldId: selectFieldId,
+                        value: true,
+                        ignoreFieldChange: true
+                    });
+                }
+
+                // Automatically deselect the checkbox if payment amount is 0
+                if (paymentAmount === 0 && isSelected) {
+                    record.setCurrentSublistValue({
+                        sublistId: sublistId,
+                        fieldId: selectFieldId,
+                        value: false,
+                        ignoreFieldChange: true
+                    });
+                }
             }
+        }
+    }
+
+    function pageInit(context) {
+        // Ensure page setup logic remains intact
+        window.onbeforeunload = null;
+        var nextButton = document.getElementById('custpage_next');
+        if (nextButton) {
+            nextButton.onclick = onNextClick;
+        } else {
+            console.error('Next button not found.');
         }
     }
 
@@ -138,38 +131,38 @@ define(['N/url', 'N/currentRecord'], function(url, currentRecord) {
         for (var i = 0; i < lineCount; i++) {
             var isSelected = record.getSublistValue({
                 sublistId: 'custpage_payment_sublist',
-                fieldId: 'custpage_select',
+                fieldId: 'custpage_selectfield',
                 line: i
             });
 
-            if (isSelected === true) {
+            if (isSelected) {
                 var vendorId = record.getSublistValue({
                     sublistId: 'custpage_payment_sublist',
-                    fieldId: 'custpage_name',
+                    fieldId: 'custpage_namefield',
                     line: i
                 });
 
                 var docId = record.getSublistValue({
                     sublistId: 'custpage_payment_sublist',
-                    fieldId: 'custpage_document_number',
+                    fieldId: 'custpage_documentnumberfield',
                     line: i
                 });
 
                 var amountPayed = parseFloat(record.getSublistValue({
                     sublistId: 'custpage_payment_sublist',
-                    fieldId: 'custpage_payment_amount',
+                    fieldId: 'custpage_paymentamountfield',
                     line: i
-                }));
+                })) || 0;
 
                 var docName = record.getSublistValue({
                     sublistId: 'custpage_payment_sublist',
-                    fieldId: 'custpage_document_number',
+                    fieldId: 'custpage_documentnumberfield',
                     line: i
                 });
 
                 var currency = record.getSublistValue({
                     sublistId: 'custpage_payment_sublist',
-                    fieldId: 'custpage_currency',
+                    fieldId: 'custpage_currencyfield',
                     line: i
                 });
 
@@ -194,7 +187,6 @@ define(['N/url', 'N/currentRecord'], function(url, currentRecord) {
             }
         }
 
-        // Convert doc_payed array to a comma-separated string
         for (var vendor in vendorData) {
             if (vendorData[vendor].doc_payed.length) {
                 vendorData[vendor].doc_payed = vendorData[vendor].doc_payed.join(', ');
@@ -207,17 +199,8 @@ define(['N/url', 'N/currentRecord'], function(url, currentRecord) {
             params: { vendorData: JSON.stringify(vendorData) }
         });
 
-        // Override the onbeforeunload event to prevent the pop-up
         window.onbeforeunload = null;
-
         window.location.href = redirectUrl;
-    }
-
-    function pageInit() {
-        // Override the onbeforeunload event to prevent the pop-up
-        window.onbeforeunload = null;
-
-        document.getElementById('custpage_next').onclick = onNextClick;
     }
 
     return {
